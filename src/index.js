d9840abb4d8c8efe09403ef01f7d4a146534532c
Christopher Mena <mena.christopher.student@gmail.com>
2023-03-23 23:28:02 -0400

    Remove boilerplate and added code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
+
+const BookList = () => {
+    return (
+        <section className="booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+}
+
+const Book = () => {
+    return (
+        <article className="book">
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
+}
+
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg" alt="Dog Man: Twenty Thousand Fleas Under the Sea"/>
+const Title = () => {
+    return <h2>Dog Man: Twenty Thousand Fleas Under the Sea</h2>;
+}
+const Author = () => <h4>Dav Pilkey</h4>
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+root.render(<BookList />);
