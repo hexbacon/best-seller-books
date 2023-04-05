8d9d5bda50eb6bb454002629ec84a23a4026f2e4
Christopher Mena <mena.christopher.student@gmail.com>
2023-04-05 01:12:09 -0400

    Complete app

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book.jpg"
+import img2 from "./images/book2.jpg"
+import img3 from "./images/book3.jpg"
+export const books = [
+    {
+    title: "Dog Man: Twenty Thousand Fleas Under the Sea",
+    author: "Dav Pilkey",
+    img: img1,
+    id: 1,
+    },
+    {
+    title: "Life Worth Living: A Guide to What Matters Most",
+    author: "Miroslav Volf, Matthew Croasmun, Ryan McAnnally-Linz",
+    img: img2,
+    id: 2,
+    }, 
+    {
+    title: "How to Catch the Easter Bunny",
+    author: "Adam Wallace (Author), Andy Elkerton (Illustrator)",
+    img: img3,
+    id: 3,
+    }
+]
+
--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
+import { books } from "./books"
+import Book from "./Book";
 
 const BookList = () => {
     return (
+        <>
+        <h1 className="page-title">amazon best sellers</h1>
         <section className="booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+        {
+            books.map((book, index) => {
+                return <Book {...book} key={book.id} number={index}
+                />;
+            })
+        }
         </section>
-    );
-}
-
-const Book = () => {
-    return (
-        <article className="book">
-            <Image />
-            <Title />
-            <Author />
-        </article>
+        </>
     );
 }
 
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg" alt="Dog Man: Twenty Thousand Fleas Under the Sea"/>
-const Title = () => {
-    return <h2>Dog Man: Twenty Thousand Fleas Under the Sea</h2>;
-}
-const Author = () => <h4>Dav Pilkey</h4>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
